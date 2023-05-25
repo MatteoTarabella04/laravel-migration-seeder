52a68f9eb1aad79a37ac2cf0947993e9454ee5f2
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-25 15:44:39 +0200

    create seeder and use faker for compile elements

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        for ($i = 0; $i < 10; $i++) {
+
+            $train = new Train();
+            $train->company = $faker->randomElement(['Trenitalia', 'Italo']);
+            $train->start_station = $faker->city();
+            $train->arrive_station = $faker->city();
+            $train->start_time = $faker->time();
+            $train->arrive_time = $faker->time();
+            $train->coaches_no = $faker->numberBetween(1, 9);
+            $train->on_time = $faker->boolean();
+            $train->cancelled = $faker->boolean();
+        }
+
+    }
+}
